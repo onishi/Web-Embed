d648232276cb03d5a683e8c80a1f2cf25322d4af
onishi <yasuhiro.onishi@gmail.com>
2012-06-23 14:54:57 +0900

    flickr

--- lib/Web/Embed/Response.pm
+++ lib/Web/Embed/Response.pm
@@ -27,7 +27,7 @@ sub new_from_uri {
 sub render {
     my $self = shift;
     if (my $oembed = $self->oembed) {
-        return $oembed->render;
+        render_oembed($oembed);
     }
 }
 
@@ -36,6 +36,38 @@ sub oembed {
     $self->{_oembed} ||= eval {
         $self->oembed_consumer->embed($self->uri);
     };
+}
+
+sub render_oembed {
+    my $response = shift;
+    if ($response->type eq 'photo') {
+        if ($response->{web_page}) {
+            my $element = HTML::Element->new('a', href => $response->{web_page});
+            $element->attr(title => $response->title) if defined $response->title;
+            my $img     = HTML::Element->new(
+                'img',
+                src    => $response->url,
+            );
+            $img->attr(alt => $response->title) if defined $response->title;
+            $element->push_content($img);
+            return $element->as_HTML;
+        } else {
+            my $img = HTML::Element->new(
+                'img',
+                src    => $response->url,
+            );
+            $img->attr(alt => $response->title) if defined $response->title;
+            return $img->as_HTML;
+        }
+    }
+    if ($response->type eq 'link') {
+        my $element = HTML::Element->new('a', href => $response->url);
+        $element->push_content(defined $response->title ? $response->title : $response->url);
+        return $element->as_HTML;
+    }
+    if ($response->html) {
+        return $response->html;
+    }
 }
 
 1;
--- t/02_base.t
+++ t/02_base.t
@@ -17,7 +17,7 @@ no  warnings qw/redefine once/;
 local *LWP::UserAgent::get = sub {
     my ($self, $url) = @_;
     diag $url;
-    (my $file = $url) =~ s{[:/.]+}{_}g;
+    (my $file = $url) =~ s{[:/.\?&]+}{_}g;
     $file = "$FindBin::Bin/stub/$file";
     diag $file;
     my $content = read_file($file) or die("no stub fo $url");
@@ -42,3 +42,10 @@ https://twitter.com/kentaro/status/13430016778
 --- expected
 <blockquote class="twitter-tweet" lang="ja"><p>アニソンとか聴いて喜んでいるひとが心底かわいそうに思います。反感を抱かれるでしょうけど、劣っていると思いますよ。</p>&mdash; kentaroさん (@kentaro) <a href="https://twitter.com/kentaro/status/13430016778" data-datetime="2010-05-05T14:42:21+00:00">5月 5, 2010</a></blockquote>
 <script src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+=== flickr
+--- input
+http://www.flickr.com/photos/btaphoto/42185736/in/photostream
+--- expected
+<a href="http://www.flickr.com/photos/btaphoto/42185736/" title="DSC_0852"><img alt="DSC_0852" src="src" /></a>
+
